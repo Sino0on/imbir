04c49e089cc9ab5a3a7067cfcc7e32a89686a2df
Dastan <dastan.kubanuulu1@gmail.com>
2026-06-27 21:23:33 +0600

    fix value props css

--- core/asgi.py
+++ core/asgi.py
@@ -1,10 +1,11 @@
 import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.middleware import JWTAuthMiddleware
 from chat.routing import websocket_urlpatterns
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
